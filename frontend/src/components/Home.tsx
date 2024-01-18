843c97f92ff9d6e164b910f35dcf4572db7038cb
vsiago <vsiago23@gmail.com>
2024-01-18 18:15:05 -0300

    Feat: atualizacao

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,7 +2,7 @@ import "../index.css";
 
 export default function Home() {
   return (
-    <div className="bg-[#243B1E] min-h-screen text-white flex items-center flex-col px-10">
+    <div className="bg-[#243B1E] min-h-screen text-white flex items-center flex-col px-8">
       <header className="flex flex-col items-center gap-4 bg-slate-300 h-60">
         <div className="bg-[#58854C] h-32 w-full absolute z-0 left-0 top 0"></div>
         <div className="flex items-center gap-5 absolute left-4 mt-20">
@@ -19,34 +19,34 @@ export default function Home() {
       </header>
       <main className="flex-1 w-full">
         <section className="w-full mt-10 rounded-lg grid grid-cols-3 gap-3 items-center justify-center">
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               Meta e <br />
               Progresso
             </p>
           </div>
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               Agradecimentos
             </p>
           </div>
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               Fotos e Vídeos
             </p>
           </div>
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               História <br />
               Pessoal
             </p>
           </div>
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               Depoimentos
             </p>
           </div>
-          <div className="bg-[#58854C] border-2 border-[#85D186] h-[105px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
+          <div className="bg-[#58854C] border-2 border-[#85D186] h-[110px] rounded-3xl flex justify-center items-center text-[#DCFFDC] text-center">
             <p className="text-[11px] uppercase leading-3 font-semibold">
               Transparência
             </p>

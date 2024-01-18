bc2893adfeb89cb0c5d2f253ceacb6745edcd713
vsiago <vsiago22@gmail.com>
2024-01-18 16:48:38 -0300

    teste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 import SplashScreen from './components/SplashScreen';
 import Home from './components/Home';
 
